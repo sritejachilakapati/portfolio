b6cd180d9e3e436b063b0a118474c2121b43ecd2
Sriteja R Chilakapati <sritejachilakapati@gmail.com>
2025-02-26 22:07:58 +0530

    chore: version upgrades

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,7 @@
-import adapter from "@sveltejs/adapter-auto";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-
   kit: {
     adapter: adapter(),
   },
